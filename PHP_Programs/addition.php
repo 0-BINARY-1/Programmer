d67449f28286631bd0462ecfb9dd7cede56e28c0
Santosh Bhat <blackcodealgorithm01@gmail.com>
2026-01-29 10:35:25 +0545

    Addition of two numbers in PHP.

---
+++ PHP_Programs/addition.php
@@ -0,0 +1,12 @@
+<?php
+
+// Here is the simple addition program in PHP with default values.
+
+$a = 5;                                 // declaring the variables
+$b = 10;
+
+$sum = $a + $b;                         //Calculating the sum
+
+echo "The sum of $a and $b is: $sum\n"; //Displaying the result
+
+?>
