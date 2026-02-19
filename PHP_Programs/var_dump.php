81fa4ba4a62abf6f74b1a66b1d27b3277b3f58c5
Santosh Bhat <blackcodealgorithm01@gmail.com>
2026-02-19 15:15:38 +0545

    php program to show output using var_dump which display output with its datatype.

---
+++ PHP_Programs/var_dump.php
@@ -0,0 +1,16 @@
+<?php
+
+// Declaring integer variable
+$x = 5; 
+
+// Declaring string variable
+$y = "Hello, World!";
+
+// Declaring float variable
+$z = 5.55;
+
+// Using var_dump to display the type and value of the variables
+var_dump("The datatype and value of x is: $x");
+var_dump("The datatype and value of y is: $y");
+var_dump("The datatype and value of z is: $z");
+?>
